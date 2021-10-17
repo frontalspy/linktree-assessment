787909e2eb24b3a60642da9424b8b60a7bceb6d5
Brendon <frontalspy@gmail.com>
2021-10-17 15:06:40 +1100

    Adds new removeSymbols func

--- src/shared/text.utils.ts
+++ src/shared/text.utils.ts
@@ -1,3 +1,7 @@
 export const capitalise = (content: string) => {
   return content.charAt(0).toUpperCase() + content.slice(1);
 };
+
+export const removeSymbols = (content: string) => {
+  return content.replace(/[-_&]/g, " ");
+};
